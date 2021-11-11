68380e31a694b8a5e82b0fa31ca839bfd44243a0
unknown <khomanesuraj8@gmail.com>
2021-11-11 09:59:43 +0530

    calculate total fare

--- src/main/java/com/invoice/InvoiceGenerator.java
+++ src/main/java/com/invoice/InvoiceGenerator.java
@@ -1,5 +1,17 @@
 package com.invoice;
 
 public class InvoiceGenerator {
+	// variable declare and assign
+	private int costPerTime = 1;
+	private double minimumCostPerKilometer = 10;
+	private double minimumFare = 5;
 
+	// method: calculate fare
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * minimumCostPerKilometer + time * costPerTime;
+		if (totalFare < minimumFare) {
+			return minimumFare;
+		}
+		return totalFare;
+	}
 }
--- src/test/java/com/invoice/InvoiceGeneratorTest.java
+++ src/test/java/com/invoice/InvoiceGeneratorTest.java
@@ -1,5 +1,36 @@
 package com.invoice;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceGeneratorTest {
+
+	// --------test for return total fare-------------
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		// object creation
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		// variable declare and assign
+		double distance = 2.0;
+		int time = 5;
+		// calculate total fare
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		double expected = 25;
+		Assert.assertEquals(expected, fare, 0.0);
+	}
+
+	// -------test for return minimum fare-------------
+	@Test
+	public void GivenLessDistanceAndTime_ShouldReturnMinFare() {
+		// object creation
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		// variable declare and assign
+		double distance = 0.1;
+		int time = 1;
+		// calculate total fare
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		double expected = 5;
+		Assert.assertEquals(expected, fare, 0.0);
+	}
 
 }
